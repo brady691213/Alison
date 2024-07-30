b771913eda21f62ec81017f2d23f333ff404bca7
Brady Kelly <brady691213@outlook.com>
2024-07-30 18:33:05 +0200

    Presentable grid for board

--- pacman/app.js
+++ pacman/app.js
@@ -1,22 +1,23 @@
 const board = ["pink", "blue", "green", "red", "purple", "orange"];
 const myBoard = [];
 const tempBoard = [
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 2, 2, 2, 2, 2, 2, 3, 2, 2, 1],
-    [1, 2, 1, 3, 1, 1, 1, 1, 1, 2, 1],
-    [1, 2, 1, 1, 1, 1, 1, 1, 1, 2, 1],
-    [1, 2, 1, 1, 1, 3, 1, 1, 1, 2, 1],
-    [1, 2, 1, 1, 1, 1, 1, 1, 1, 2, 1],
-    [1, 2, 1, 1, 3, 1, 1, 1, 1, 2, 1],
-    [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 2, 2, 2, 2, 2, 2, 3, 2, 1],
+    [1, 2, 2, 3, 2, 2, 2, 4, 2, 1],
+    [1, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+    [1, 2, 3, 2, 2, 3, 2, 2, 2, 1],
+    [1, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+    [1, 2, 2, 2, 3, 2, 2, 2, 2, 1],
+    [1, 2, 2, 2, 2, 3, 2, 2, 2, 1],
+    [1, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
 ];
 const ghosts = [];
 const g = {
     x: "",
     y: "",
-    h: 100,
-    size: 25,
+    cellSize: 100,
+    size: 10,
     size: tempBoard.length,
     ghosts: 5,
     inplay: false
@@ -28,38 +29,37 @@ const player = {
     pause: false
 }
 
-function createBox(templateName) {
+function createBox(templateName, row, col) {
     let newCell = document.querySelector(`#${templateName}-template`).cloneNode(true);
     newCell.attributes.removeNamedItem("id");
+    newCell.classList.add(`cell-${row},${col}`);
+    newCell.classList.add("dynamic");
     g.grid.appendChild(newCell);
     myBoard.push(newCell);
     return newCell;
 }
-function createSquare(val) {
+function createSquare(val, row, col) {
     switch (val) {
-        case 1: return createBox("wall");
-        case 2: return createBox("dot");
-        case 3: return createBox("superdot");
+        case 1: return createBox("wall", row, col);
+        case 2: return createBox("dot", row, col);
+        case 3: return createBox("superdot", row, col);
+        case 4: return createBox("pacman", row, col);
+        default: return createBox("square", row, col);
     }
     console.log("CreateSquare: " + val);
 }
 
 function createGame() {
 
-    for(r = 0; r < 9; r++) {
-        let row = tempBoard[r];
-        for(c = 0; c < 9; c++) {
-            let col = row[c];
-            createSquare(col);
-            // if (g.grid.querySelectorAll(".boxStart").length > 100) {
-            //     debugger;
-            // }
+    for(r = 0; r < tempBoard.length; r++) {
+        for(c = 0; c < tempBoard[r].length; c++) {
+            createSquare(tempBoard[r][c], r, c);
         }
     }
 
     let colWidths = [];
-    for (let i = 0; i <= g.size; i++) {
-        colWidths.push(g.h);
+    for (let i = 0; i < g.size; i++) {
+        colWidths.push(g.cellSize);
     }
     g.x = colWidths.map(w => `${w}px`).join(' ');
 
