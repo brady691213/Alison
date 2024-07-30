94627babf2f6d71bac872805ece095b8f096c81e
Brady Kelly <brady691213@outlook.com>
2024-07-30 17:44:26 +0200

    Recovered from mystery bug

--- pacman/app.js
+++ pacman/app.js
@@ -1,15 +1,15 @@
 const board = ["pink", "blue", "green", "red", "purple", "orange"];
 const myBoard = [];
 const tempBoard = [
-    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-    1, 2, 2, 2, 2, 2, 2, 3, 2, 2, 1,
-    1, 2, 1, 3, 1, 1, 1, 1, 1, 2, 1,
-    1, 2, 1, 1, 1, 1, 1, 1, 1, 2, 1,
-    1, 2, 1, 1, 1, 3, 1, 1, 1, 2, 1,
-    1, 2, 1, 1, 1, 1, 1, 1, 1, 2, 1,
-    1, 2, 1, 1, 3, 1, 1, 1, 1, 2, 1,
-    1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1,
-    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 2, 2, 2, 2, 2, 2, 3, 2, 2, 1],
+    [1, 2, 1, 3, 1, 1, 1, 1, 1, 2, 1],
+    [1, 2, 1, 1, 1, 1, 1, 1, 1, 2, 1],
+    [1, 2, 1, 1, 1, 3, 1, 1, 1, 2, 1],
+    [1, 2, 1, 1, 1, 1, 1, 1, 1, 2, 1],
+    [1, 2, 1, 1, 3, 1, 1, 1, 1, 2, 1],
+    [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
 ];
 const ghosts = [];
 const g = {
@@ -17,6 +17,7 @@ const g = {
     y: "",
     h: 100,
     size: 25,
+    size: tempBoard.length,
     ghosts: 5,
     inplay: false
 }
@@ -27,40 +28,44 @@ const player = {
     pause: false
 }
 
-// function createSquare(val) {
-//     const div = document.createElement("div");
-//     div.classList.add("box");
-//     if (val === 1) {
-//         div.classList.add("wall");
-//     }
-//     if (val === 2) {
-//         const dot = document.createElement("div");
-//         dot.classList.add("dot");
-//         div.append(dot);
-//     }
-//     if (val === 3) {
-//         const dot = document.createElement("div");
-//         dot.classList.add("superdot");
-//         div.append(dot);
-//     }
-//     g.grid.append(div);
-//     myBoard.push(div);
-// }
+function createBox(templateName) {
+    let newCell = document.querySelector(`#${templateName}-template`).cloneNode(true);
+    newCell.attributes.removeNamedItem("id");
+    g.grid.appendChild(newCell);
+    myBoard.push(newCell);
+    return newCell;
+}
+function createSquare(val) {
+    switch (val) {
+        case 1: return createBox("wall");
+        case 2: return createBox("dot");
+        case 3: return createBox("superdot");
+    }
+    console.log("CreateSquare: " + val);
+}
 
-// function createGame() {
-//     for (let cell of tempBoard) {
-//         createSquare(cell);
-//     }
-//
-//     let colWidths = [];
-//     for (let i = 0; i < g.size; i++) {
-//         colWidths.push(g.h);
-//     }
-//     g.x = colWidths.map(w => `${w}px`).join(' ');
-//
-//     g.grid.style.gridTemplateColumns = g.x;
-//     g.grid.style.gridTemplateRows = g.x;
-// }
+function createGame() {
+
+    for(r = 0; r < 9; r++) {
+        let row = tempBoard[r];
+        for(c = 0; c < 9; c++) {
+            let col = row[c];
+            createSquare(col);
+            // if (g.grid.querySelectorAll(".boxStart").length > 100) {
+            //     debugger;
+            // }
+        }
+    }
+
+    let colWidths = [];
+    for (let i = 0; i <= g.size; i++) {
+        colWidths.push(g.h);
+    }
+    g.x = colWidths.map(w => `${w}px`).join(' ');
+
+    g.grid.style.gridTemplateColumns = g.x;
+    g.grid.style.gridTemplateRows = g.x;
+}
 
 document.addEventListener("DOMContentLoaded", () => {
     g.grid = document.querySelector(".grid");
@@ -69,9 +74,7 @@ document.addEventListener("DOMContentLoaded", () => {
     g.mouth = document.querySelector(".mouth");
     g.ghost = document.querySelector(".ghost");
     //g.ghost.style = "display: none";
-
-    //createGame();
-
+    createGame();
     console.log(g);
 })
 
